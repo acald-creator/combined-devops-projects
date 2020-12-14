ae87c0481364e443d5682b9b8e7a328920dc1721
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-14 17:56:31 -0600

    Setup the rest api to get a person name and post a person's name

--- course-02/exercises/c2-basic-server/src/server.ts
+++ course-02/exercises/c2-basic-server/src/server.ts
@@ -10,6 +10,39 @@ app.get('/', async(req: Request, res: Response) => {
     res.send("Welcome to the Cloud!")
 })
 
+/* Greet the person with the name
+* http://localhost:8082/persons/{name}
+*/
+app.get('/persons/:name', async(req: Request, res: Response) => {
+    let { name } = req.params
+    if (!name) {
+        return res.status(400).send(`Name is required`)
+    }
+    return res.status(200).send(`Welcome to the Cloud, ${name}!`)
+})
+
+/* Greet a specific person to demonstrate req.query
+* http://localhost:8082/persons?name={name}
+*/
+app.get('/persons/', (req: Request, res: Response) => {
+    let { name } = req.query
+    if(!name) {
+        return res.status(400).send(`Name is required`)
+    }
+    return res.status(200).send(`Welcome to the cloud, ${name}!`)
+})
+
+/* Post a greeting to the specific person
+*
+*/
+app.post('/persons', async(req: Request, res: Response) => {
+    let { name } = req.query
+    if (!name) {
+        return res.status(400).send(`Name is required`)
+    }
+    return res.status(200).send(`Welcome to the cloud, ${name}!`)
+})
+
 app.listen(port, () => {
     console.log(`server running http://localhost:${port}`)
     console.log(`press CTRL+C to stop server`)
