285a971dd1b5f37c3209d07ba9e674539dafaec6
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-14 16:54:29 -0600

    Add bodyparser

--- course-02/exercises/c2-basic-server/src/server.ts
+++ course-02/exercises/c2-basic-server/src/server.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express"
+import bodyParser from 'body-parser'
 
 const app = express()
 const port = 8082
+
+app.use(bodyParser.json())
 
 app.get('/', async(req: Request, res: Response) => {
     res.send("Welcome to the Cloud!")
