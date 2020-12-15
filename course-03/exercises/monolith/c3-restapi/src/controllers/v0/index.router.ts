367edbbfe39171b3a30943def09169696da3c99a
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-15 16:14:14 -0600

    Fixed the file with eslint

--- course-03/exercises/monolith/c3-restapi/src/controllers/v0/index.router.ts
+++ course-03/exercises/monolith/c3-restapi/src/controllers/v0/index.router.ts
@@ -1,14 +1,14 @@
-import { Router, Request, Response } from 'express'
-import { FeedRouter } from './feed/routes/feed.router'
-import { UserRouter } from './users/routes/user.router'
+import { Router, Request, Response } from 'express';
+import { FeedRouter } from './feed/routes/feed.router';
+import { UserRouter } from './users/routes/user.router';
 
-const router: Router = Router()
+const router: Router = Router();
 
-router.use('/feed', FeedRouter)
-router.use('/user', UserRouter)
+router.use('/feed', FeedRouter);
+router.use('/user', UserRouter);
 
 router.get('/', async (req: Request, res: Response) => {
-    res.send(`V0`)
-})
+  res.send('V0');
+});
 
-export const IndexRouter: Router = router
+export const IndexRouter: Router = router;
