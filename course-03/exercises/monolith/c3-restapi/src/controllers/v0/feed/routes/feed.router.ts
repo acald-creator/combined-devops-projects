8411a3b224fa50285357f2a89312562017374dc0
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-15 16:12:56 -0600

    Fixed the file with eslint

--- course-03/exercises/monolith/c3-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-03/exercises/monolith/c3-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -6,58 +6,58 @@ import * as AWS from '../../../../aws';
 const router: Router = Router();
 /* Retrieve all feed items */
 router.get('/', async (req: Request, res: Response) => {
-    const items = await FeedItem.findAndCountAll({ order: [['id', 'DESC']] });
-    items.rows.map((item) => {
-        if (item.url) {
-            item.url = AWS.getGetSignedUrl(item.url);
-        }
-    });
-    res.send(items);
+  const items = await FeedItem.findAndCountAll({ order: [['id', 'DESC']] });
+  items.rows.map((item) => {
+    if (item.url) {
+      item.url = AWS.getGetSignedUrl(item.url);
+    }
+  });
+  res.send(items);
 });
 /* Retrieve a specific item */
 router.get('/:id', async (req: Request, res: Response) => {
-    let { id } = req.params
-    const item = await FeedItem.findByPk(id)
-    res.send(item)
-})
+  const { id } = req.params;
+  const item = await FeedItem.findByPk(id);
+  res.send(item);
+});
 /* Update a specific item */
 router.patch('/:id',
-    requireAuth,
-    async (req: Request, res: Response) => {
-        res.send(500).send("Not implemented")
-    });
+  requireAuth,
+  async (req: Request, res: Response) => {
+    res.send(500).send('Not implemented');
+  });
 /* Get a signed url to put a new item into the S3 bucket */
 router.get('/signed-url/:fileName',
-    requireAuth,
-    async (req: Request, res: Response) => {
-        let { fileName } = req.params;
-        const url = AWS.getPutSignedUrl(fileName);
-        res.status(201).send({ url: url });
-    });
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { fileName } = req.params;
+    const url = AWS.getPutSignedUrl(fileName);
+    res.status(201).send({ url });
+  });
 
 router.post('/',
-    requireAuth,
-    async (req: Request, res: Response) => {
-        const caption = req.body.caption;
-        const fileName = req.body.url;
-        /* Check if the caption is valid */
-        if (!caption) {
-            return res.status(400).send({ message: 'Caption is required or malformed' });
-        }
-        /* Check if the filename is valid */
-        if (!fileName) {
-            return res.status(400).send({ message: 'File url is required' });
-        }
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { caption } = req.body;
+    const fileName = req.body.url;
+    /* Check if the caption is valid */
+    if (!caption) {
+      return res.status(400).send({ message: 'Caption is required or malformed' });
+    }
+    /* Check if the filename is valid */
+    if (!fileName) {
+      return res.status(400).send({ message: 'File url is required' });
+    }
 
-        const item = new FeedItem({
-            caption: caption,
-            url: fileName
-        });
-
-        const savedItem = await item.save();
-
-        savedItem.url = AWS.getGetSignedUrl(savedItem.url);
-        res.status(201).send(savedItem);
+    const item = new FeedItem({
+      caption,
+      url: fileName,
     });
+
+    const savedItem = await item.save();
+
+    savedItem.url = AWS.getGetSignedUrl(savedItem.url);
+    res.status(201).send(savedItem);
+  });
 
 export const FeedRouter: Router = router;
