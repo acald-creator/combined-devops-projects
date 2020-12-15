a8064aaa2d79a7ff211a823ed15128a383818cd8
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-15 16:13:05 -0600

    Fixed double to single

--- course-03/exercises/monolith/c3-restapi/src/controllers/v0/users/models/Users.ts
+++ course-03/exercises/monolith/c3-restapi/src/controllers/v0/users/models/Users.ts
@@ -1,33 +1,32 @@
 import {
-    Table,
-    Column,
-    Model,
-    PrimaryKey,
-    CreatedAt,
-    UpdatedAt,
-  } from "sequelize-typescript";
-  
+  Table,
+  Column,
+  Model,
+  PrimaryKey,
+  CreatedAt,
+  UpdatedAt,
+} from 'sequelize-typescript';
+
   @Table
-  export class User extends Model<User> {
+export class User extends Model<User> {
     @PrimaryKey
     @Column
     public email!: string;
-  
+
     @Column
     public passwordHash!: string; // for nullable fields
-  
+
     @Column
     @CreatedAt
     public createdAt: Date = new Date();
-  
+
     @Column
     @UpdatedAt
     public updatedAt: Date = new Date();
-  
+
     short() {
       return {
         email: this.email,
       };
     }
-  }
-  
+}
