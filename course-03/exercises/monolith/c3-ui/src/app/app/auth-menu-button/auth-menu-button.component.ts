3934b753023f9d5648a589101d60fea7c6d44540
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-15 20:07:01 -0600

    Generated auth-menu-button

---
+++ course-03/exercises/monolith/c3-ui/src/app/app/auth-menu-button/auth-menu-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-menu-button',
+  templateUrl: './auth-menu-button.component.html',
+  styleUrls: ['./auth-menu-button.component.scss'],
+})
+export class AuthMenuButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
