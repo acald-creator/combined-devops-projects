073171bc91c9ebb8f2be94c3308fd25c270eeccc
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-15 20:56:31 -0600

    Add feed module

--- course-03/exercises/monolith/c3-ui/src/app/home/home.module.ts
+++ course-03/exercises/monolith/c3-ui/src/app/home/home.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
+import { FeedModule } from '../feed/feed.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    FeedModule,
     FormsModule,
     IonicModule,
     HomePageRoutingModule
