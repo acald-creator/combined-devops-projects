43652f386205afbd8711cfd369e1d47c56f04e49
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-15 20:41:28 -0600

    Moved the folders to the right places

---
+++ course-03/exercises/monolith/c3-ui/src/app/feed/feed.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FeedProviderService } from './services/feed.provider.service'
+import { FeedUploadComponent } from './feed-upload/feed-upload.component';
+import { FeedListComponent } from './feed-list/feed-list.component';
+import { FeedItemComponent } from './feed-item/feed-item.component';
+
+const entryComponents = [FeedUploadComponent]
+const components = [FeedListComponent, FeedItemComponent, FeedUploadComponent]
+
+@NgModule({
+  declarations: components,
+  imports: [
+    CommonModule
+  ],
+  exports: components,
+  entryComponents: entryComponents,
+  providers: [FeedProviderService]
+})
+export class FeedModule { }
