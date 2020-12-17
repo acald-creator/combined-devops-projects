3d399f4ef2a16dd0dd8e08d489457981dc6fda4f
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-17 17:58:22 -0600

    Setup the environment to pull in the backend

--- course-03/exercises/monolith/c3-ui/src/app/api/api.service.ts
+++ course-03/exercises/monolith/c3-ui/src/app/api/api.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const API_HOST = environment.apiHost;
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +12,43 @@ export class ApiService {
     headers: new HttpHeaders({'Content-Type': 'application/json' })
   };
 
+  token: string;
+
   constructor(private http: HttpClient) { }
 
   handleError(error: Error){
     alert(error.message);
+  }
+
+  setAuthToken(token) {
+    this.httpOptions.headers = this.httpOptions.headers.append('Authorization', `jwt ${token}`);
+    this.token = token;
+  }
+
+  async get(endpoint): Promise<any> {
+    const url = `${API_HOST}${endpoint}`;
+    const req = this.http.get(url, this.httpOptions).pipe(map(this.extractData));
+
+    try {
+      return req.toPromise();
+    } catch (e) {
+      this.handleError(e);
+      throw e;
+    }
+  }
+
+  async post(endpoint, data): Promise<any> {
+    const url = `${API_HOST}${endpoint}`;
+    try {
+      return this.http.post<HttpEvent<any>>(url, data, this.httpOptions).toPromise();
+    } catch (e) {
+      this.handleError(e);
+      throw e;
+    }
+  }
+
+  private extractData(res: HttpEvent<any>) {
+    const body = res;
+    return body || { };
   }
 }
