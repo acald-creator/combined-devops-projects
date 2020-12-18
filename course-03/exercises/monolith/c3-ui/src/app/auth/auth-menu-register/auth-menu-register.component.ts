5ebbba52354441c24ed80e542d1b1aaf83bd276d
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-17 20:41:54 -0600

    Setup the form to register

--- course-03/exercises/monolith/c3-ui/src/app/auth/auth-menu-register/auth-menu-register.component.ts
+++ course-03/exercises/monolith/c3-ui/src/app/auth/auth-menu-register/auth-menu-register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
+import { group } from 'console';
+import { User } from '../models/user.model';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-auth-menu-register',
@@ -7,8 +12,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthMenuRegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  error: string;
 
-  ngOnInit() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private auth: AuthService,
+    private modal: ModalController
+  ) { }
 
-}
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      passwordConfirm: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9.]+$')
+      ])),
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+$')
+      ]))
+    }, { validators: this.passwordsMatch });
+  }
+
+  onSubmit($event) {
+    $event.preventDefault();
+
+    if (!this.registerForm.valid) { return; }
+
+    const newUser: User = {
+      email: this.registerForm.controls.email.value,
+      name: this.registerForm.controls.name.value
+    };
+
+    this.auth.register(newUser, this.registerForm.controls.password.value).then((user) => {
+      this.modal.dismiss();
+    }).catch((e) => {
+      this.error = e.statusText;
+      throw e;
+    });
+  }
+    passwordsMatch(group: FormGroup) {
+      return group.controls.password.value === group.controls.passwordConfirm.value ? null : { passwordMisMatch: true };
+    }
+  }
+
