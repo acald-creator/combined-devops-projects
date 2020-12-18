b0260a08c5bcf0a85848f62ff34de0a3283398f5
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-17 18:16:37 -0600

    Set up the auth guard, service and module

--- course-03/exercises/monolith/c3-ui/src/app/auth/auth.module.ts
+++ course-03/exercises/monolith/c3-ui/src/app/auth/auth.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthMenuButtonComponent } from './auth-menu-button/auth-menu-button.component';
+import { AuthMenuRegisterComponent } from './auth-menu-register/auth-menu-register.component';
+import { AuthLoginComponent } from './auth-login/auth-login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { ApiModule } from '../api/api.module';
 
-
+const entryComponents = [AuthMenuButtonComponent, AuthMenuRegisterComponent, AuthLoginComponent];
+const components = [...entryComponents];
 
 @NgModule({
-  declarations: [],
+  entryComponents,
+  declarations: entryComponents,
+  exports: components,
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule,
+    ApiModule
   ]
 })
 export class AuthModule { }
--- course-03/exercises/monolith/c3-ui/src/app/auth/services/auth.service.ts
+++ course-03/exercises/monolith/c3-ui/src/app/auth/services/auth.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ApiService } from 'src/app/api/api.service';
+import { User } from '../models/user.model';
+
+const JWT_LOCALSTORE_KEY = 'jwt';
+const USER_LOCALSTORE_KEY = 'user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+    currentUser$: BehaviorSubject<User> = new BehaviorSubject<User>(null);
+    constructor(private api: ApiService) {
+      this.initToken();
+    }
 
-  constructor() { }
+    initToken() {
+      const token = localStorage.getItem(JWT_LOCALSTORE_KEY);
+      const user = JSON.parse(localStorage.getItem(USER_LOCALSTORE_KEY)) as User;
+      if (token && user ) {
+        this.setTokenAndUser(token, user);
+      }
+    }
+
+    setTokenAndUser(token: string, user: User) {
+      localStorage.setItem(JWT_LOCALSTORE_KEY, token);
+      localStorage.setItem(USER_LOCALSTORE_KEY, JSON.stringify(user));
+      this.api.setAuthToken(token);
+      this.currentUser$.next(user);
+    }
+
+    async login(email: string, password: string): Promise<any> {
+      return this.api.post('/usrs/auth/login', {
+        email, password
+      }).then((res) => {
+        this.setTokenAndUser(res.token, res.user);
+        return res;
+      }).catch((e) => { throw email; });
+    }
+
+    logout(): boolean {
+      this.setTokenAndUser(null, null);
+      return true;
+    }
+
+    register(user: User, password: string): Promise<any> {
+      return this.api.post('/users/auth', {
+        email: user.email, password
+      }).then((res) => {
+        this.setTokenAndUser(res.token, res.user);
+        return res;
+      }).catch((e) => { throw e; });
+    }
 }
