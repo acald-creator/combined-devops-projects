88ca0811da0ea884ebb87c32f9f989cb6fd03771
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-17 20:41:25 -0600

    Finished the rest of auth menu button component

--- course-03/exercises/monolith/c3-ui/src/app/auth/auth-menu-button/auth-menu-button.component.ts
+++ course-03/exercises/monolith/c3-ui/src/app/auth/auth-menu-button/auth-menu-button.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { AuthLoginComponent } from '../auth-login/auth-login.component';
+import { AuthMenuRegisterComponent } from '../auth-menu-register/auth-menu-register.component';
+import { AuthService } from '../services/auth.service';
+import { AuthMenuUserComponent } from './auth-menu-user/auth-menu-user.component';
 
 @Component({
   selector: 'app-auth-menu-button',
@@ -7,7 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthMenuButtonComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private modelController: ModalController
+  ) { }
+
+  async presentmodal(ev: any) {
+    const modal = await this.modelController.create({
+      component: AuthMenuUserComponent
+    });
+    return await modal.present();
+  }
+
+  async presentLogin(ev: any) {
+    const modal = await this.modelController.create({
+      component: AuthLoginComponent,
+    });
+    return await modal.present();
+  }
+
+  async presentRegister(ev: any) {
+    const modal = await this.modelController.create({
+      component: AuthMenuRegisterComponent
+    });
+    return await modal.present();
+  }
+
+  logout() {
+    this.auth.logout;
+  }
 
   ngOnInit() {}
 
