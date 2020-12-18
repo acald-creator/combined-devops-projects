4fe23eab313a7eede98201e077b7dd83dc97e375
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-17 22:01:12 -0600

    Added the appname environment

--- course-03/exercises/monolith/c3-ui/src/app/home/home.page.ts
+++ course-03/exercises/monolith/c3-ui/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  appName = environment.appName;
 
   constructor() {}
 
